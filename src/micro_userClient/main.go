86ff474a65263ece138ba9a83fcdc52d6478b63c
安增辉 <35863525+ZengHuiAn@users.noreply.github.com>
2019-11-18 09:33:58 +0800

    aa

--- src/micro_userClient/main.go
+++ src/micro_userClient/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	micro "github.com/micro/go-micro"
+	"github.com/micro/go-micro/client"
 	customProto "redisProject/build/proto"
 )
 
@@ -12,9 +13,26 @@ import (
 //	return nil
 //}
 
+type logWrapper struct {
+	client.Client
+}
+
+func (l *logWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	fmt.Printf("[wrapper] client request to service: %s endpoint: %s\n", req.Service(), req.Endpoint())
+	return l.Client.Call(ctx, req, rsp)
+}
+
+// implements client.Wrapper as logWrapper
+func logWrap(c client.Client) client.Client {
+	return &logWrapper{c}
+}
+
 func main() {
 	// Create a new service
-	service := micro.NewService(micro.Name("greeter.client"))
+	service := micro.NewService(
+		micro.Name("greeter.client"),
+		micro.WrapClient(logWrap),
+		)
 	// Initialise the client and parse command line flags
 	service.Init()
 
@@ -22,13 +40,13 @@ func main() {
 	userClient := customProto.NewUserService("UserService", service.Client())
 
 	p := micro.NewPublisher("events", service.Client())
-	ctx :=context.TODO()
+	ctx := context.TODO()
 
-	p.Publish(ctx,&customProto.Event{
-		Id:"11",
+	p.Publish(ctx, &customProto.Event{
+		Id: "11",
 	})
 	// Call the greeter
-	rsp, err := userClient.CreateUser(context.TODO(), &customProto.CreateUserRequest{UserName: "snake",Passwd:"123"})
+	rsp, err := userClient.CreateUser(context.TODO(), &customProto.CreateUserRequest{UserName: "snake", Passwd: "123"})
 	if err != nil {
 		fmt.Println(err)
 	}
--- src/micro_userService/main.go
+++ src/micro_userService/main.go
@@ -4,9 +4,11 @@ import (
 "context"
 "fmt"
 micro "github.com/micro/go-micro"
-customProto "redisProject/build/proto"
+	"github.com/micro/go-micro/server"
+	customProto "redisProject/build/proto"
+	"time"
 
-"redisProject/src/business"
+	"redisProject/src/business"
 )
 
 func ProcessEvent(ctx context.Context, event *customProto.Event) error {
@@ -14,9 +16,17 @@ func ProcessEvent(ctx context.Context, event *customProto.Event) error {
 	return nil
 }
 
+func logWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		fmt.Printf("[%v] server request: %s\n", time.Now(), req.Endpoint())
+		return fn(ctx, req, rsp)
+	}
+}
+
 func main() {
 	service := micro.NewService(
 		micro.Name("UserService"),
+		micro.WrapHandler(logWrapper),
 	)
 
 	service.Init()
