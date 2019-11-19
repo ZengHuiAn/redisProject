3969fdca868a2143902c62f8e154c4f14cc7876e
安增辉 <35863525+ZengHuiAn@users.noreply.github.com>
2019-11-19 09:53:39 +0800

    utils

---
+++ client/Assets/Scripts/Util/LogTool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogTool 
+{
+    public string BytesToString(byte[] buffer)
+    {
+
+        if (buffer == null)
+        {
+            return "";
+        }
+        string str = "";
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            str += (buffer[i]+" ");
+        }
+
+        return str;
+    }
+
+    public void LogBytes(byte[] buffer)
+    {
+        Debug.Log(this.BytesToString(buffer));
+    }
+
+    private static LogTool instance;
+    public static LogTool Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new LogTool();
+            }
+
+            return instance;
+        }
+    }
+}
