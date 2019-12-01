c3c0fb8c7bd50791df62053441ea7df6f1065f62
安增辉 <35863525+ZengHuiAn@users.noreply.github.com>
2019-12-01 23:34:58 +0800

    xlua init

---
+++ client/Assets/Scripts/Lua/LuaController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class LuaController : MonoBehaviour
+{
+    private LuaEnv _luaEnv;
+
+    public LuaEnv L
+    {
+        get { return _luaEnv; }
+    }
+
+    private static LuaController instance;
+    
+    
+    public event Action onAwake ;
+    void Awake()
+    {
+        if (instance !=null)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        instance = this;
+        
+        _luaEnv = new LuaEnv();
+        
+        L.AddLoader(FileUtils.LuaLoader);
+        
+        L.DoString("require 'init'");
+
+
+        var MainAction = L.Global.Get<System.Action>("main");
+        if (MainAction != null)
+        {
+            MainAction();
+        }
+    }
+}
---
+++ client/Assets/Scripts/Util/FileUtils.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FileUtils
+    {
+
+        public static string ReadFile(string path, string fileName, string suffix = ".txt")
+        {
+            string content = System.IO.File.ReadAllText(path + fileName + suffix);
+
+            return content;
+        }
+
+        public static string envPath
+        {
+            get
+            {
+                string filepath = "";
+#if UNITY_EDITOR
+                filepath = UnityEngine.Application.dataPath + "/Lua/";
+
+#elif UNITY_IPHONE
+	        filepath = Application.dataPath +"/Raw/Lua/";
+ 
+#elif UNITY_ANDROID
+	        filepath = "jar:file://" + Application.dataPath + "!/assets/Lua/";
+ 
+#endif
+                return filepath;
+            }
+        }
+
+        public static byte[] LuaLoader(ref string fileName)
+        {
+            string convertName = fileName.Replace('.', '/');
+            string filepath;
+#if UNITY_EDITOR
+            filepath = UnityEngine.Application.dataPath + "/../../Lua/Client/";
+
+#elif UNITY_IPHONE
+	        filepath = Application.dataPath +"/Raw/Lua/";
+ 
+#elif UNITY_ANDROID
+	        filepath = "jar:file://" + Application.dataPath + "!/assets/Lua/";
+ 
+#endif
+            string content = FileUtils.ReadFile(filepath, convertName, ".lua");
+
+            return System.Text.Encoding.UTF8.GetBytes(content);
+        }
+
+        public static string ReadFileContent(string fileName)
+        {
+            string convertName = fileName.Replace('.', '/');
+            string filepath;
+#if UNITY_EDITOR
+            filepath = UnityEngine.Application.dataPath + "/../../Lua/Client/";
+
+#elif UNITY_IPHONE
+	        filepath = Application.dataPath +"/Raw/Lua/";
+ 
+#elif UNITY_ANDROID
+	        filepath = "jar:file://" + Application.dataPath + "!/assets/Lua/";
+ 
+#endif
+            string content = FileUtils.ReadFile(filepath, convertName, ".lua");
+            return content;
+        }
+        
+        /*
+         * 获取相对路径
+         */
+        public static string GetRelativePath(string path)
+        {
+            if (!path.Contains(Application.dataPath))
+            {
+                return path;
+            }
+            
+            var relativePath = path.Substring(Application.dataPath.Length - 6);
+
+            return relativePath;
+        }
+
+    }
