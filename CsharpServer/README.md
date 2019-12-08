2c1205e9f6041c4138ee3876dcb8423ead3be03d
安增辉 <35863525+ZengHuiAn@users.noreply.github.com>
2019-12-08 16:13:58 +0800

    拷贝文件和readme

---
+++ CsharpServer/README.md
@@ -0,0 +1,10 @@
+## NetLib 文件夹是动态库
+
+
+## NetClient 是测试的客户端
+
+
+## Server 是服务器端
+
+
+## proto 是协议protobuffer 文件
---
+++ CsharpServer/copyToClient.bat
@@ -0,0 +1,2 @@
+copy /Y .\NetLib\bin\Debug\NetLib.dll ..\client\Assets\Plugins\Protocol\NetLib.dll
+copy /Y .\NetLib\bin\Debug\NetLib.pdb ..\client\Assets\Plugins\Protocol\NetLib.pdb
